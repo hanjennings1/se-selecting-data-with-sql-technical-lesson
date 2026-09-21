8e437b4b150a2bbbb7c5665b512ff0a8e81aa582
Hannah Jennings <han.jennings1@gmail.com>
2026-09-21 12:17:37 -0400

    practice with SELECT in SQL

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+import sqlite3
+import pandas as pd
 
+conn = sqlite3.connect('data.sqlite')
+
+orders = pd.read_sql("""
+        SELECT orderDate,
+        strftime('%m', orderDate) AS month,
+        strftime('%Y', orderDate) AS year,
+        strftime('%d', orderDate) AS day
+        FROM orders;
+""", conn)
+
+print(orders)
+
+conn.close()
